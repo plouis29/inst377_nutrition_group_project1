f609fe2ce0c05e16c5cb018ba843f69bb5370c2f
plouis29 <159855937+plouis29@users.noreply.github.com>
2024-05-19 20:15:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const supabaseUrl = 'https://mkglwaoohztcetlvucik.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZ2x3YW9vaHp0Y2V0bHZ1Y2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MjEyMjgsImV4cCI6MjAzMTM5NzIyOH0.nNVK_uKZjBX4YwkTNWS2Js_H9cT3A-uxdQMJwBrJdBs'
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey)
 
+// VERCEL: Allows front end
+app.get('/', (req, res) => {
+    res.sendFile('public/Home.html', { root: __dirname })
+})
+
 // Creating the API
 // Source for the data is from: https://www.health.harvard.edu/blog/10-superfoods-to-boost-a-healthy-diet-2018082914463
 app.get('/superfood', async (req, res) => {
